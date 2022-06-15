014bb493dcb390854b243ff10b74aba95168768a
Valery <66918953+vzahurski@users.noreply.github.com>
2022-06-15 16:12:18 +0300

    rest_example (Project example Spring Boot)

---
+++ rest_example/src/test/java/simple_java_projects/rest_example/RestExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package simple_java_projects.rest_example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
