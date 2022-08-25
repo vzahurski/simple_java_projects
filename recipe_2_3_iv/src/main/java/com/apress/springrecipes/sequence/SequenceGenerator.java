f3a571e06867d863b5b0bad03bda9e670b571102
Valery <66918953+vzahurski@users.noreply.github.com>
2022-08-25 23:42:14 +0300

    recipe_2_3_iv @Autowired Deinum Spring 5 Recipes 2017.pdf

---
+++ recipe_2_3_iv/src/main/java/com/apress/springrecipes/sequence/SequenceGenerator.java
@@ -0,0 +1,41 @@
+package com.apress.springrecipes.sequence;
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class SequenceGenerator {
+    private PrefixGenerator prefixGenerator;
+    private String suffix;
+    private int initial;
+    private int counter;
+
+    public SequenceGenerator() {
+    }
+    public SequenceGenerator(PrefixGenerator prefixGenerator, String suffix, int initial) {
+        this.prefixGenerator = prefixGenerator;
+        this.suffix = suffix;
+        this.initial = initial;
+    }
+    //@Autowired
+    public void setPrefixGenerator(PrefixGenerator prefixGenerator) {
+        this.prefixGenerator = prefixGenerator;
+    }
+    @Autowired
+    public void myOwnCustomInjectionName(PrefixGenerator prefixGenerator) {
+        this.prefixGenerator = prefixGenerator;
+    }
+    // Интересно, что помечая метод не конструктор  @Autowired мы говорим спрингу, чтобы он вызвал этот
+    // метод и подставил в него необходимые параметры
+    public void setSuffix(String suffix) {
+        this.suffix = suffix;
+    }
+    public void setInitial(int initial) {
+        this.initial = initial;
+    }
+
+    public synchronized String getSequence() {
+        StringBuilder builder = new StringBuilder();
+        builder.append(prefixGenerator.getPrefix());
+        builder.append(initial + counter++);
+        builder.append(suffix);
+        return builder.toString();
+    }
+}
